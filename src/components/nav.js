43b701b52e7407ebe321b906c0b7dc70d069d44a
Stephanie Wiggins <stephawiggins@gmail.com>
2021-08-27 09:14:52 -0500

    fix styles in migrating to Bootstrap 5

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,8 +7,8 @@ const SiteNav = () => {
   return (
     <Navbar>
       <Nav.Link href="/"><img src={SWLogo} alt="Logo" /></Nav.Link>
-      <Nav.Link href="/#about" className="link-animate mr-3" >About</Nav.Link>
-      <Nav.Link href="/#portfolio" className="link-animate mr-3">Portfolio</Nav.Link>
+      <Nav.Link href="/#about" className="link-animate me-3" >About</Nav.Link>
+      <Nav.Link href="/#portfolio" className="link-animate me-3">Portfolio</Nav.Link>
       <Nav.Link href="/#contact" className="link-animate"> Connect</Nav.Link>
     </Navbar>
   )
